4bf04686ac3bf3f3bfe9860f2d82acee6c91073f
aharshsingh <aharshsingh25@gmail.com>
2025-07-06 22:21:12 +0530

    feat: refreshAPI

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.post("/signup", authController.register);
 router.post("/login", authController.login);
-
+router.post("/refresh", authController.refresh);
 export default router;
 
